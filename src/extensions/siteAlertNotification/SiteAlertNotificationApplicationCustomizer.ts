738e4ef49d001ff605f168c966b345c23b9ccede
Paul Summers <paulsumm@microsoft.com>
2019-10-09 22:33:39 -0400

    Functional albeit with bad style

--- src/extensions/siteAlertNotification/SiteAlertNotificationApplicationCustomizer.ts
+++ src/extensions/siteAlertNotification/SiteAlertNotificationApplicationCustomizer.ts
@@ -5,7 +5,8 @@ import {
   PlaceholderContent,
   PlaceholderName
 } from '@microsoft/sp-application-base';
-import { Dialog } from '@microsoft/sp-dialog';
+
+import {SPHttpClient, SPHttpClientResponse} from '@microsoft/sp-http';
 
 import * as strings from 'SiteAlertNotificationApplicationCustomizerStrings';
 import styles from './SiteAlertNotification.module.scss';
@@ -21,6 +22,14 @@ const LOG_SOURCE: string = 'SiteAlertNotificationApplicationCustomizer';
 export interface ISiteAlertNotificationApplicationCustomizerProperties {
   // This is an example; replace with your own property
   topData: string;
+}
+
+export interface ISiteAlertMessage {
+  NotificationLevel: string;
+  NotificationMessage: string;
+  StartDateTime: Date;
+  EndDateTime: Date;
+  MoreInformationLink: string;
 }
 
 /** A Custom Action which can be run during execution of a Client Side Application */
@@ -41,6 +50,8 @@ export default class SiteAlertNotificationApplicationCustomizer
   private _renderPlaceHolders(): void {
     console.log("Rendering top placeholder data");
 
+    
+
     if(!this._topPlaceHolder){
       this._topPlaceHolder = this.context.placeholderProvider.tryCreateContent(
         PlaceholderName.Top,
@@ -53,12 +64,50 @@ export default class SiteAlertNotificationApplicationCustomizer
       }
     }
 
-    if(this._topPlaceHolder.domElement) {
-      this._topPlaceHolder.domElement.innerHTML = `
-        <div class="${styles.app}">
-          <div class="${styles.top}">Posting a message in the app</div>
-        </div>`;
-    }
+    this._getCurrentNotification()
+    .then((notificationMessages) => {
+      if(notificationMessages) {
+        console.log(`Notification found: ${notificationMessages[0].NotificationMessage}`);
+        return notificationMessages[0];        
+      }
+      else {
+        console.log('No notifications found. Returning nothing.');
+        return undefined;
+      }
+    })
+    .then((notification: ISiteAlertMessage): void => {
+      if(notification) {
+        let notificationStyle = styles.notification;
+        if(notification.NotificationLevel == "Emergency") notificationStyle = styles.emergency;
+        if(notification.NotificationLevel == "Warning") notificationStyle = styles.warning;
+
+        if(this._topPlaceHolder.domElement) {
+          this._topPlaceHolder.domElement.innerHTML = `
+            <div class="${styles.app}">
+              <div class="${styles.top} ${notificationStyle}">${notification.NotificationMessage}</div>
+            </div>`;
+        }
+      }
+      else {
+        console.log(`No notifications found. Not rendering header`);
+      }
+    });
+
+        
+  }
+
+  private _getCurrentNotification(): Promise<ISiteAlertMessage[]> {
+
+    let currentDateTime: string = new Date().toISOString();
+    let requestUrl = this.context.pageContext.web.absoluteUrl + `/_api/web/lists/getByTitle('Notifications')/items?$filter=StartDate le datetime'${currentDateTime}' and EndDateTime ge datetime'${currentDateTime}'&$select=NotificationMessage,NotificationLevel,StartDate,EndDateTime,MoreInformationLink`;
+
+    return this.context.spHttpClient.get(
+      requestUrl, SPHttpClient.configurations.v1
+    ).then((res: SPHttpClientResponse): Promise<{value: ISiteAlertMessage[]}> => {
+      return res.json();
+    }).then((res: {value: ISiteAlertMessage[]}): ISiteAlertMessage[] => {
+      return res.value;
+    });
   }
 
   private _onDispose(): void {
